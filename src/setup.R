afbf7c7f3ec03c6e2cb744876581e31596a363ae
danielhong1982 <daniel.bongki.hong@gmail.com>
2023-11-29 17:07:25 +0900

    Adding scripts and notebook files.

---
+++ src/setup.R
@@ -0,0 +1,7 @@
+.libPaths()
+renv::init()
+install.packages(c("tidyverse", "remotes"))
+remotes::install_github("giocomai/ganttrify")
+install.packages("readxl")
+install.packages("MetBrewer")
+install.packages("Cairo")
